15434d75b5ef5d992d033dea35507470a74fba72
Khai <khai.pnp@outlook.com>
2022-02-23 17:30:29 +0700

    'news'

--- pages/detail-news/index.js
+++ pages/detail-news/index.js
@@ -9,6 +9,9 @@ export default function DetailNews() {
   const [desc, setDesc] = useState(
     'Khán giả phản ứng về việc vợ chồng David Beckham để con trai Cruz chụp hình theo phong cách nổi loạn và xăm mình khi chưa đủ tuổi quy định'
   )
+  const [descImg, setDescImg] = useState(
+    'Vợ chồng Beckham bị chỉ trích vì chia sẻ ảnh chụp theo phong cách nổi loạn của con trai.'
+  )
   const [content, setContent] = useState(
     'Ngày 22/2, vợ chồng Beckham đã chia sẻ ảnh chụp cho tạp chí i-D của con trai Cruz Beckham lên trang cá nhân. Cặp vợ chồng nổi tiếng đăng tổng cộng 13 bức ảnh với chú thích: “Ngày đầu tiên của tuổi 17 thật tuyệt vời. Mẹ tự hào về con". <br />Tuy nhiên, sau khi đăng tải loạt ảnh của con trai, Vic-Becks đã vấp phải làn sóng chỉ trích từ khán giả. Nhiều người cho rằng hình ảnh nổi loạn trong bộ ảnh không phù hợp với Cruz. Cậu nhuộm tóc hồng, cởi trần và mặc quần cạp trễ lộ viền nội y. Một số ảnh, Cruz thậm chí chỉ mặc đồ lót.'
   )
@@ -17,9 +20,6 @@ export default function DetailNews() {
   useEffect(() => {
     setTimePost(currentTime)
   }, [])
-
-  // Title news
-  //   document.title = title
 
   return (
     <>
@@ -33,15 +33,16 @@ export default function DetailNews() {
         <div className="content">
           <h6 className="font-bold">{desc}</h6>
           <p>{content}</p>
-          <Image
-            src={'img/Cruz_Beckham_q_1645503379.jpeg'}
-            width={400}
-            height={400}
-            className="border-gray-600 border-solid rounded"
-            placeholder={
-              'Vợ chồng Beckham bị chỉ trích vì chia sẻ ảnh chụp theo phong cách nổi loạn của con trai.'
-            }
-          />
+          <div>
+            <Image
+              src={'/img/Cruz_Beckham_q_1645503379.jpeg'}
+              width={400}
+              height={400}
+              className="border-gray-600 border-solid rounded"
+              placeholder={descImg}
+            />
+            <p>{descImg}</p>
+          </div>
         </div>
       </div>
     </>
